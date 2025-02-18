a8c977b47b8de5ad8eab6e907a2dda22151c6570
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2025-02-18 16:26:58 +0700

    added sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -77,3 +78,10 @@ tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 
+sonar {
+    properties {
+        property("sonar.projectKey", "samuellapnadia_eshop")
+        property("sonar.organization", "samuellapnadia")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
