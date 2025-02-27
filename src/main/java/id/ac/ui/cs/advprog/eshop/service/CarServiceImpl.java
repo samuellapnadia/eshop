21948da37d177bdcce287fdbbe59c51b337ccf21
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2025-02-27 14:01:58 +0700

    DIP - Changing the service so that it depends on abstraction

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -1,7 +1,8 @@
+// File: CarServiceImpl.java
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Car;
-import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
+import id.ac.ui.cs.advprog.eshop.repository.CarRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
@@ -12,11 +13,10 @@ import java.util.List;
 public class CarServiceImpl implements CarService {
 
     @Autowired
-    private CarRepository carRepository;
+    private CarRepositoryInterface carRepository;
 
     @Override
     public Car create(Car car) {
-        // TODO Auto-generated method stub
         carRepository.create(car);
         return car;
     }
@@ -31,19 +31,16 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public Car findById(String carId) {
-        Car car = carRepository.findById(carId);
-        return car;
+        return carRepository.findById(carId);
     }
 
     @Override
     public void update(String carId, Car car) {
-        // TODO Auto-generated method stub
         carRepository.update(carId, car);
     }
 
     @Override
     public void deleteCarById(String carId) {
-        // TODO Auto-generated method stub
         carRepository.delete(carId);
     }
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -1,10 +1,9 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
-import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -13,7 +12,7 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductRepositoryInterface productRepository;
 
     @Override
     public Product create(Product product) {
@@ -28,19 +27,16 @@ public class ProductServiceImpl implements ProductService {
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
     }
-
     // Finds the product based on ID
     @Override
     public Product findById(String id) {
         return productRepository.findById(id);
     }
-
     // Updates the product based on the intended ID
     @Override
     public void update(String id, Product product) {
         productRepository.update(id, product);
     }
-
     // Deletes the product based on the intended ID
     @Override
     public void delete(String id) {
