48317010437236a5d557808aca0685a42c778b9b
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2025-03-07 19:02:24 +0700

    [REFACTOR] refactoring PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,11 +1,13 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class PaymentRepository {
     private final List<Payment> paymentList;
 
